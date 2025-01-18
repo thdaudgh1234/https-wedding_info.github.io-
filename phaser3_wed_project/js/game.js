d0f46e49bc7072ea2eb8e57f753ad65a5a838c9f
thdaudgh1234 <songmoungho@hanmail.net>
2025-01-18 23:42:24 +0900

    Update game.js

--- phaser3_wed_project/js/game.js
+++ phaser3_wed_project/js/game.js
@@ -29,7 +29,7 @@ class Example extends Phaser.Scene {
 
         const goal = this.physics.add.staticImage(this.scale.width / 2 + 60, this.scale.height / 3 - 80, 'goal').setDepth(1);
 
-        const cannonHead = this.add.image(this.scale.width / 2, this.scale.height - 100, 'cannon_head').setDepth(1);
+        const cannonHead = this.add.image(this.scale.width / 2, this.scale.height - 70, 'cannon_head').setDepth(1);
         const cannon = this.add.image(this.scale.width / 2, this.scale.height, 'cannon_body').setDepth(0);
 
         const graphics = this.add.graphics({ lineStyle: { width: 10, color: 0xffdd00, alpha: 0.5 } });
@@ -74,7 +74,7 @@ class Example extends Phaser.Scene {
 
         // 포인터 클릭 시 발사
         this.input.on('pointerup', () => {
-            const chick = chicks.get(cannon.x, cannon.y - 150); // 그룹에서 발사체 가져오기
+            const chick = chicks.get(cannon.x, cannon.y - 120); // 그룹에서 발사체 가져오기
             if (chick) {
                 chick.setActive(true);
                 chick.setVisible(true);
