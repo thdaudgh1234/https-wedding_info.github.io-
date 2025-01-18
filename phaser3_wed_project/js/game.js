68abfdceac570c671cfd6d0c070755b694ad264e
thdaudgh1234 <songmoungho@hanmail.net>
2025-01-18 23:52:29 +0900

    game up

--- phaser3_wed_project/js/game.js
+++ phaser3_wed_project/js/game.js
@@ -172,7 +172,8 @@ const config = {
     pixelArt: true, // 픽셀 아트 최적화
     resolution: window.devicePixelRatio, // 고해상도 지원
     scale: {
-        mode: Phaser.Scale.FIT, // 화면 크기에 맞게 조정
+        //mode: Phaser.Scale.FIT, // 화면 크기에 맞게 조정
+		mode: Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.CENTER_BOTH, // 화면 중앙 정렬
     },
     physics: {
