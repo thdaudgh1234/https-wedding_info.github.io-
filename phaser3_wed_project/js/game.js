e5143db5bc4a3836ecdd0209ee6a62dda535b91a
thdaudgh1234 <songmoungho@hanmail.net>
2025-01-22 15:39:48 +0900

    Update game.js

--- phaser3_wed_project/js/game.js
+++ phaser3_wed_project/js/game.js
@@ -43,7 +43,7 @@ class Example extends Phaser.Scene {
 		
 		// 바닥 생성
 		const floor = this.physics.add.staticImage(this.scale.width / 2, this.scale.height, 'wall_fail') // wall_fail 이미지를 바닥으로 사용
-			.setOrigin(0.5, 0.0) // 하단 중심 기준으로 설정
+			.setOrigin(0.5, 1.0) // 하단 중심 기준으로 설정
 			.setDisplaySize(this.scale.width, 10) // 바닥의 너비를 화면 크기로 설정
 			.refreshBody(); // 물리 엔진에 업데이트
 
