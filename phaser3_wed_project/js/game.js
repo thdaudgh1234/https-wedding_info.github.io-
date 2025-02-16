b92bb512399eb0f4157dc0f1991aeba8b1735763
thdaudgh1234 <songmoungho@hanmail.net>
2025-02-16 15:12:00 +0900

    Update game.js

--- phaser3_wed_project/js/game.js
+++ phaser3_wed_project/js/game.js
@@ -297,6 +297,7 @@ class Example extends Phaser.Scene {
 		);
 		// origin을 중앙 상단으로 설정
 		wallGoalSprite.setOrigin(0.5, 0);
+		wallGoalSprite.body.setOffset(0, -20);
 
 		// 애니메이션 시작
 		wallGoalSprite.anims.play('wall_goal_spr', true);
